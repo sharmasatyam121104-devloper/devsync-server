461fa0b62442ad072cdf2229675bde201d057de4
sharmasatyam121104-devloper <sharma.satyam121104@gmail.com>
2026-03-09 23:12:47 +0530

    chore: properly initialize Express server with TypeScript setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 import {requestLogger} from './utils/logger'
 
 const app = express()
 
+
+app.use(cors({
+  origin: process.env.CLIENT,
+  credentials: true
+}))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(requestLogger)
 
 app.get("/", (req, res) => {
